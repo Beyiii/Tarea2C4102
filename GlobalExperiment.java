7532fef3763f456e004352c81e572872c03178d1
cpereiram <cristobal.pereira@ug.uchile.cl>
2024-11-10 18:38:48 -0300

    Update GlobalExperiment.java

--- GlobalExperiment.java
+++ GlobalExperiment.java
@@ -1,15 +1,20 @@
 
 
 public class GlobalExperiment {
-    private static final int[] N_VALUES = {100000}; //, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000};
+    private static final int[] N_VALUES = {800000, 900000, 1000000};
     
     Experiment experiment = new Experiment();
 
     public void runExperiments() {
         for (int N : N_VALUES) {
             int M = 100 * N; // Número de búsquedas
-            experiment.ejecutarExperimento(N, M);
+            Experiment.ejecutarExperimento(N, M);
         }
+    }
+
+    public static void main(String[] args) {
+        GlobalExperiment globalExperiment = new GlobalExperiment();
+        globalExperiment.runExperiments();
     }
 
 }
